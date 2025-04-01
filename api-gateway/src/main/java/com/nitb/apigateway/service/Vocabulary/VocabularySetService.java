b774385cb31a4e65fc856510c127bb3569317a39
Test_repo <22520992@gm.uit.edu.vn>
2025-04-01 22:25:56 +0700

    Add Vocabulary Service

--- api-gateway/src/main/java/com/nitb/apigateway/service/Vocabulary/VocabularySetService.java
+++ api-gateway/src/main/java/com/nitb/apigateway/service/Vocabulary/VocabularySetService.java
@@ -1,7 +1,19 @@
 package com.nitb.apigateway.service.Vocabulary;
 
+import com.nitb.apigateway.dto.Vocabulary.request.CreateVocabularySetRequestDto;
+import com.nitb.apigateway.dto.Vocabulary.request.UpdateVocabularySetRequestDto;
+import com.nitb.apigateway.dto.Vocabulary.response.VocabularySetDetailResponseDto;
+import com.nitb.apigateway.dto.Vocabulary.response.VocabularySetResponseDto;
+import com.nitb.apigateway.dto.Vocabulary.response.VocabularySetsPaginationResponseDto;
 import reactor.core.publisher.Mono;
 
-public interface VocabularySetService {
+import java.util.UUID;
 
+public interface VocabularySetService {
+    Mono<VocabularySetDetailResponseDto> createVocabularySet(CreateVocabularySetRequestDto request);
+    Mono<VocabularySetDetailResponseDto> getVocabularySetById(UUID id);
+    Mono<VocabularySetsPaginationResponseDto> getVocabularySets(int page, int size);
+    Mono<VocabularySetsPaginationResponseDto> searchVocabularySetByName(String keyword, int page, int size);
+    Mono<VocabularySetResponseDto> updateVocabularySet(UUID id, UpdateVocabularySetRequestDto request);
+    Mono<VocabularySetResponseDto> deleteVocabularySet(UUID id);
 }
