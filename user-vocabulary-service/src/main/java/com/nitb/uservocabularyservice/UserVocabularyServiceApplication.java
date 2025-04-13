7edeb70bc5d0ae0518b05dcb5cee46c43fafd8a3
Test_repo <22520992@gm.uit.edu.vn>
2025-04-13 12:51:56 +0700

    Add User Vocabulary module

---
+++ user-vocabulary-service/src/main/java/com/nitb/uservocabularyservice/UserVocabularyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nitb.uservocabularyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserVocabularyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserVocabularyServiceApplication.class, args);
+    }
+
+}
