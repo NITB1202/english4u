e320021f8a4d186b975dadde6483158ce9f17b85
Test_repo <22520992@gm.uit.edu.vn>
2025-04-16 11:13:30 +0700

    Add Cors config

--- api-gateway/src/main/java/com/nitb/apigateway/dto/UserVocabulary/response/SavedSetDetailResponseDto.java
+++ api-gateway/src/main/java/com/nitb/apigateway/dto/UserVocabulary/response/SavedSetDetailResponseDto.java
@@ -1,6 +1,5 @@
 package com.nitb.apigateway.dto.UserVocabulary.response;
 
-import com.nitb.apigateway.dto.Vocabulary.response.VocabularySetResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
