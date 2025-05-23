7ec6c9fa31a5328b03eac9474b7ce0e67fa9c9c2
Test_repo <22520992@gm.uit.edu.vn>
2025-05-23 18:15:26 +0700

    refactor(test): update services

--- api-gateway/src/main/java/com/nitb/apigateway/service/Test/PartService.java
+++ api-gateway/src/main/java/com/nitb/apigateway/service/Test/PartService.java
@@ -1,20 +1,11 @@
 package com.nitb.apigateway.service.Test;
 
-import com.nitb.apigateway.dto.General.ActionResponseDto;
-import com.nitb.apigateway.dto.Test.request.Part.CreatePartRequestDto;
-import com.nitb.apigateway.dto.Test.request.Part.UpdatePartRequestDto;
 import com.nitb.apigateway.dto.Test.response.Part.PartDetailResponseDto;
-import com.nitb.apigateway.dto.Test.response.Part.PartResponseDto;
-import com.nitb.apigateway.dto.Test.response.Part.PartSummaryResponseDto;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface PartService {
-    Mono<PartResponseDto> addPartToTest(UUID userId, UUID testId, CreatePartRequestDto request);
-    Mono<PartDetailResponseDto> getPartDetailById(UUID id);
-    Mono<List<PartSummaryResponseDto>> getAllPartsForTest(UUID testId);
-    Mono<ActionResponseDto> updatePart(UUID id, UUID userId, UpdatePartRequestDto request);
-    Mono<ActionResponseDto> swapPartsPosition(UUID userId, UUID part1Id, UUID part2Id);
+    Mono<List<PartDetailResponseDto>> getAllPartsForTest(UUID testId);
 }
