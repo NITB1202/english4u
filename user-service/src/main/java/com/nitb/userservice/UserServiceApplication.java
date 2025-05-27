976b3e4adc26e4ab46bfe2328047d4236bbe356f
Test_repo <22520992@gm.uit.edu.vn>
2025-05-27 17:41:32 +0700

    feat: add config files

--- user-service/src/main/java/com/nitb/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/nitb/userservice/UserServiceApplication.java
@@ -3,7 +3,7 @@ package com.nitb.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.nitb.userservice", "com.nitb.common"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
