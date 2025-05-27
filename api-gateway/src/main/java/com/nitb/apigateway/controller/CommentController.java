6fc45bd2f705f2d08ca44e6510d17dee24005952
Test_repo <22520992@gm.uit.edu.vn>
2025-05-27 10:02:03 +0700

    feat(comment): complete comment controller

---
+++ api-gateway/src/main/java/com/nitb/apigateway/controller/CommentController.java
@@ -0,0 +1,56 @@
+package com.nitb.apigateway.controller;
+
+import com.nitb.apigateway.dto.Test.Comment.request.PostCommentRequestDto;
+import com.nitb.apigateway.dto.Test.Comment.request.ReplyCommentRequestDto;
+import com.nitb.apigateway.dto.Test.Comment.response.CommentResponseDto;
+import com.nitb.apigateway.dto.Test.Comment.response.CommentsResponseDto;
+import com.nitb.apigateway.exception.ErrorResponse;
+import com.nitb.apigateway.service.Test.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/comments")
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping
+    @Operation(summary = "Post a comment.")
+    @ApiResponse(responseCode = "200", description = "Post successfully.")
+    @ApiResponse(responseCode = "400", description = "Invalid request body.",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    public Mono<ResponseEntity<CommentResponseDto>> postComment(@RequestParam UUID userId,
+                                                                @Valid @RequestBody PostCommentRequestDto request) {
+        return commentService.postComment(userId, request).map(ResponseEntity::ok);
+    }
+
+    @PostMapping("/reply")
+    @Operation(summary = "Reply a comment.")
+    @ApiResponse(responseCode = "200", description = "Reply successfully.")
+    @ApiResponse(responseCode = "400", description = "Invalid request body.",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    public Mono<ResponseEntity<CommentResponseDto>> replyComment(@RequestParam UUID userId,
+                                                                 @Valid @RequestBody ReplyCommentRequestDto request) {
+        return commentService.replyComment(userId, request).map(ResponseEntity::ok);
+    }
+
+    @GetMapping
+    @Operation(summary = "Get a paginated list of comments.")
+    @ApiResponse(responseCode = "200", description = "Get successfully.")
+    public Mono<ResponseEntity<CommentsResponseDto>> getComments(@RequestParam UUID userId,
+                                                                 @Positive(message = "Page must be positive") @RequestParam int page,
+                                                                 @RequestParam(defaultValue = "10") int size) {
+        return commentService.getComments(userId, page, size).map(ResponseEntity::ok);
+    }
+}
