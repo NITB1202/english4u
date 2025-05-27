2095d3b8a7bf16b24a0f34bd5023e37e76249da2
Test_repo <22520992@gm.uit.edu.vn>
2025-05-27 18:21:09 +0700

    feat: add user gRPC

---
+++ api-gateway/src/main/java/com/nitb/apigateway/grpc/UserGrpcClient.java
@@ -0,0 +1,79 @@
+package com.nitb.apigateway.grpc;
+
+import com.google.protobuf.Empty;
+import com.nitb.common.grpc.ActionResponse;
+import com.nitb.userservice.grpc.*;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserGrpcClient {
+    @GrpcClient("user-service")
+    private UserServiceGrpc.UserServiceBlockingStub blockingStub;
+
+    public Empty checkCanPerformAction(UUID userId) {
+        CheckCanPerformActionRequest request = CheckCanPerformActionRequest.newBuilder()
+                .setUserId(userId.toString())
+                .build();
+
+        return blockingStub.checkCanPerformAction(request);
+    }
+
+    public ActionResponse createUser(String name) {
+        CreateUserRequest request = CreateUserRequest.newBuilder()
+                .setName(name)
+                .build();
+
+        return blockingStub.createUser(request);
+    }
+
+    public UserResponse getUserById(UUID id) {
+        GetUserByIdRequest request = GetUserByIdRequest.newBuilder()
+                .setId(id.toString())
+                .build();
+
+        return blockingStub.getUserById(request);
+    }
+
+    public UsersResponse getUsersByListOfIds(List<UUID> ids) {
+        List<String> idsStr = ids.stream()
+                .map(UUID::toString)
+                .toList();
+
+        GetUsersByListOfIdsRequest request = GetUsersByListOfIdsRequest.newBuilder()
+                .addAllIds(idsStr)
+                .build();
+
+        return blockingStub.getUsersByListOfIds(request);
+    }
+
+    public ActionResponse updateName(UUID id, String name) {
+        UpdateNameRequest request = UpdateNameRequest.newBuilder()
+                .setId(id.toString())
+                .setName(name)
+                .build();
+
+        return blockingStub.updateName(request);
+    }
+
+    public ActionResponse updateAvatar(UUID id, String avatarUrl) {
+        UpdateAvatarRequest request = UpdateAvatarRequest.newBuilder()
+                .setId(id.toString())
+                .setAvatarUrl(avatarUrl)
+                .build();
+
+        return blockingStub.updateAvatar(request);
+    }
+
+    public ActionResponse setUserLocked(UUID id, boolean isLocked) {
+        SetUserLockedRequest request = SetUserLockedRequest.newBuilder()
+                .setId(id.toString())
+                .setIsLocked(isLocked)
+                .build();
+
+        return blockingStub.setUserLocked(request);
+    }
+}
