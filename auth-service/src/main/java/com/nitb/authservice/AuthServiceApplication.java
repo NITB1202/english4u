8ada2b3a505a7958cb88ff3c472ac475b889d67a
Test_repo <22520992@gm.uit.edu.vn>
2025-05-29 01:22:41 +0700

    feat: add config files

--- auth-service/src/main/java/com/nitb/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/nitb/authservice/AuthServiceApplication.java
@@ -3,7 +3,7 @@ package com.nitb.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.nitb.authservice", "com.nitb.common"})
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
