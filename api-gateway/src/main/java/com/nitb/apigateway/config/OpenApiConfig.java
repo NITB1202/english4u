f25f3fb276fe5f81bd496c0c01651ec3728ea78d
Test_repo <22520992@gm.uit.edu.vn>
2025-05-30 14:13:02 +0700

    feat: add security config

--- api-gateway/src/main/java/com/nitb/apigateway/config/OpenApiConfig.java
+++ api-gateway/src/main/java/com/nitb/apigateway/config/OpenApiConfig.java
@@ -1,18 +1,33 @@
 package com.nitb.apigateway.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "English4u API",
+                version = "1.0",
+                description = "API documentation for an English study website using WebFlux & gRPC"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenApiConfig {
+
     @Bean
-    public OpenAPI openAPi(){
-        return new OpenAPI()
-                .info(new Info()
-                        .title("English4u API")
-                        .version("1.0")
-                        .description("API documentation for an English study website using WebFlux & gRPC"));
+    public OpenAPI openAPI() {
+        return new OpenAPI();
     }
 }
